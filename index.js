8d23da8f1814ad7d5994206e1da5b51ed262d470
Remy Loubradou <remyloubradou@gmail.com>
2013-10-07 00:36:41 +0100

    NEW: Build a more interesting message from the http request and response.

--- index.js
+++ index.js
@@ -11,6 +11,8 @@
 
 var http = require('http'),
 	util = require('util'),
+	url = require('url'),
+	hasNestedProperty = require('hnp'),
 	STATUS_CODES = http.STATUS_CODES;
 
 /**
@@ -22,21 +24,42 @@ module.exports = HTTPError;
 
 /**
  * Initialize a `HTTPError` object
- *
- * @param {Number|String} code - HTTP status code
- *
  * @constructor
- * @extends {Error}
+ *
+ * @param {http.ClientRequest}  request -
+ * @param {http.IncomingMessage} response -
+ * @param {String} [message] - message prepended in the Error#message property,
+ *  default to empty string
+ * @inherits {Error}
  *
  * @api public
  */
 
-function HTTPError(code){
+function HTTPError(req,res,message){
 	Error.call(this);
 	Error.captureStackTrace(this,arguments.callee);
 	this.name = 'HTTPError';
-	var description = STATUS_CODES[code] || 'Unknown status code';
-	this.message = code + ' - ' + description;
+
+	var description = STATUS_CODES[res.statusCode] || 'Unknown status code';
+	req.headers = req.headers || req._headers;
+	var reqHeaders = Object.keys(req.headers).map(function(name){
+		return name + ': ' + req.headers[name]
+	});
+	var resHeaders = Object.keys(res.headers).map(function(name){
+		return name + ': ' + res.headers[name]
+	});
+
+	var protocol = 'http';
+	if(hasNestedProperty(req,'socket.pair.ssl')) protocol = 'https';
+	if(!req.uri) req.uri =  protocol + '://' + req.headers['host'] + req.path;
+
+	this.message = (message || '') + '\r\n'
+		+ 'Request URL: ' + url.format(req.uri || {}) + '\r\n'
+		+ 'Request method: ' + req.method + '\r\n'
+		+ 'Status code: ' + res.statusCode + ' - ' + description + '\r\n'
+		+ 'Request headers: \r\n' + reqHeaders.join('\r\n') + '\r\n'
+		+ 'Response headers: \r\n' + resHeaders.join('\r\n')
+		+ (res.body ? '\r\nResponse body: \r\n' + res.body : '');
 }
 
 util.inherits(HTTPError,Error);
